040893f11cfc3f15eda3ef7e3d7609824be8c83a
sdf <rhjvanworkum@gmail.com>
2022-02-27 12:20:00 +0100

    some changes

--- AudioPlayer.py
+++ AudioPlayer.py
@@ -1,16 +1,20 @@
 import pyaudio
 import wave
-import sys
+import config
 import numpy as np
 
 class AudioPlayer:
     def __init__(self, filename: str):
-        self.__chunk = 1024
         self.__filename = filename
-
         self.__wf = wave.open(filename)
+        self.__chunk = int(self.__wf.getframerate() / config.FPS)
 
         self.__p = pyaudio.PyAudio()
+
+    def convert_stereo_to_mono(self, y):
+        left = np.array([el for i, el in enumerate(y) if i % 2 == 0])
+        right = np.array([el for i, el in enumerate(y) if i % 2 != 0])
+        return right + left / 2
 
     def fft_analysis(self, data: np.ndarray)-> np.ndarray:
         # Normalize samples between 0 and 1
@@ -21,8 +25,9 @@ class AudioPlayer:
         # Pad with zeros until the next power of two
         y_padded = np.pad(y, (0, N_zeros), mode='constant')
         ys = np.abs(np.fft.rfft(y_padded)[:N // 2])
+        xs = np.fft.rfftfreq(len(y_padded), 1.0 / self.__wf.getframerate())[:N // 2]
 
-        return ys
+        return xs, ys
 
     def open_stream(self, audio_buffer_callback: callable):
 
@@ -35,17 +40,25 @@ class AudioPlayer:
         data = self.__wf.readframes(self.__chunk)
         print(f"now playing {self.__filename}")
 
-        # play stream (3)
+        # we will grap some audio data from the audio file and use it to
+        # 1. write the data to a PyAudio stream, so you can hear the audio on the speakers
+        # 2. analyze the data using FFT in order to obtain the frequencies present in the sound
+        # 3. send both this data to the audio_buffer_callback
         while len(data) > 0:
             stream.write(data)
-            y = np.frombuffer(data, dtype=np.int16)
 
             data = self.__wf.readframes(self.__chunk)
-            fft = self.fft_analysis(y)
-            audio_buffer_callback(data,fft)
+            y = np.frombuffer(data, dtype=np.int16)
 
+            if self.__wf.getnchannels() == 2:
+                y = self.convert_stereo_to_mono(y)
+
+            frequency_bins, frequency = self.fft_analysis(y)
+            audio_buffer_callback(y, frequency_bins, frequency)
+
+        # close the audio stream
         stream.stop_stream()
         stream.close()
 
         # close PyAudio (5)
-        p.terminate()
+        self.__p.terminate()
--- app.py
+++ app.py
@@ -2,31 +2,38 @@ import SendData
 import numpy as np
 import config
 import AudioPlayer
+import argparse
 
 send_data = SendData.SendData()
 
+""" 
+This is the main function to be modified by you.
 
-def audio_data_cb(audio_buffer,fft):
-    #Example: send data to the simulator 
-    #Simulator should be fully red
+It receives three inputs:
+    1. audio_buffer (np.ndarray with length 735): the raw audio data.
+    2. frequency bins (np.ndarray with length 367): the bins in which the frequency spectrum is quantitized in.
+    3. frequency (np.ndarray with length 367): the intensity of different frequencies in the currently played audio.
+
+This function is being called, in the case of config.FPS == 60, 60 times a second. This means that it receives both the raw audio data & the present
+frequencies in the audio data for only ~16.6ms of audio every time it is called. If you would modify config.FPS, this would change however.
+"""
+def audio_data_cb(audio_buffer: np.ndarray, frequency_bins: np.ndarray, frequency: np.ndarray) -> None:
+    # Example of preparing the data to be displayed in the simulator -> this should turn every pixel in the simulator red
     data = np.zeros((3, config.LENGTH))
-    data[0,:]=255
-    
+    data[0, :] = 255
 
     #input here functions to manipulate, analyze and convert the data and it to the simulator
     #data should be in the format [[RED,RED,RED...],[GREEN,GREEN...],[BLUE...]]
-
-
     send_data.send_data(data)
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("song")
+    args = parser.parse_args()
+
     #initialize the audio player
-    player = AudioPlayer.AudioPlayer("songs/MEDUZA - Tell It To My Heart.wav")
+    player = AudioPlayer.AudioPlayer(args.song)
 
     #start the audio player with callback function as input
-    player.open_stream(audio_data_cb)
-
-    #play second song
-    player = AudioPlayer.AudioPlayer("songs/MEDUZA - Tell It To My Heart.wav")
     player.open_stream(audio_data_cb)
