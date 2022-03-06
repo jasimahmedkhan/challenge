a750c0e6a60f55e8b8c7a851210b5afb361fc9ef
JASIM AHMED <jasimahmedkhan@gmail.com>
2022-03-06 23:17:24 +0100

    Uploading the solution for music visulazation task

--- app.py
+++ app.py
@@ -17,15 +17,51 @@ It receives three inputs:
 This function is being called, in the case of config.FPS == 60, 60 times a second. This means that it receives both the raw audio data & the present
 frequencies in the audio data for only ~16.6ms of audio every time it is called. If you would modify config.FPS, this would change however.
 """
+
+
+def sample_song(random, frequency, data):
+    ## Sampling values based on frequency of the song or randomness to create the visualization
+    if random:
+        # Sampling based on random values between 0 to 100 where shape of the array is (3, 100)
+        indices = np.random.randint(100, size=(3, 100))
+        data[0, indices[0, :]] = 255
+        data[1, indices[1, :]] = 255
+        data[2, indices[2, :]] = 255
+    else:
+        # Sampling based on intensity of frequencey values where, we select 300 values and later based on thresholding. 
+        values = np.random.choice(frequency, 300, replace=False)
+        red = values[:100]
+        green = values[100:200]
+        blue = values[200:300]
+        red = np.where(red > np.average(red), 255, 0).astype(float)
+        green = np.where(green > np.average(green), 255, 0).astype(float)
+        blue = np.where(blue > np.average(blue), 255, 0).astype(float)
+        data[0, :] = red
+        data[1, :] = green
+        data[2, :] = blue
+    
+    return data
+
+
 def audio_data_cb(audio_buffer: np.ndarray, frequency_bins: np.ndarray, frequency: np.ndarray) -> None:
     # Example of preparing the data to be displayed in the simulator -> this should turn every pixel in the simulator red
     data = np.zeros((3, config.LENGTH))
-    data[0, :] = 255
-
-    #input here functions to manipulate, analyze and convert the data and it to the simulator
+    # data[0, ::5] = 255
+    # data[1, :] = 255
+    # data[2, :] = 255
+    # print("data = ", data)
+        
+    # print("data length = ", data.shape)
+    
+    #input here functions to manipulate, analyze and convert the data and it to the simulator    
+    random = 0
+    sample_song(random=random, frequency=frequency, data=data)
+    
     #data should be in the format [[RED,RED,RED...],[GREEN,GREEN...],[BLUE...]]
     send_data.send_data(data)
-
+    
+    
+    
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
